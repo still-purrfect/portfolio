ed0d327dc328b356e5012a34b8e8642330c8582f
still-purrfect <maryanneakinyi408@gmaol.com>
2026-06-12 19:47:52 +0300

    portfolio ready for deployment

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contact(request):
+    return render(request, 'contact.html')
