019ce0a0c299b9fc07eab8cd3eb70393d8f42213
still-purrfect <maryanneakinyi408@gmaol.com>
2026-06-12 21:41:34 +0300

    fix static files for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i3r7^4d*&u2!oc#brcw+lqjduifopc$@b^x*o8tw!c*l1v1*f6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -114,8 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR/ "staticfiles"
