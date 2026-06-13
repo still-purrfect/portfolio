56daefbf0e23e9946be1c70646703194d2621056
still-purrfect <maryanneakinyi408@gmaol.com>
2026-06-13 23:47:08 +0300

    Restore homepage template rendering

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home.html")
+    return HttpResponse("Render is working")
 
 def about(request):
     return render(request, 'about.html')
